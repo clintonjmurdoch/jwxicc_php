c36b936d92a1a64ae05799e048b223f0b79a2c02
Clinton Murdoch <clintonjmurdoch@gmail.com>
2012-10-19 22:50:01 +1100

    testing javascripted menu

--- navbar.php
+++ navbar.php
@@ -1,8 +1,9 @@
 	<div class="hlist">
           <!-- main navigation: horizontal list -->
-          <ul>
+          <ul id="row1">
             <li><a href="index.php">Home</a></li>
-            <li><a href="matches.php?seasonselect=2010/2011">Matches</a></li>
+            <!-- <li><a href="matches.php?seasonselect=2010/2011">Matches</a></li>-->
+            <li><a onclick="showRow2('WF')">Willowfest</a></li>
             <li><a href="http://www.willowfest.com.au/results/">Ladder</a></li>
             <li><a href="playerprofiles.php">Player Profiles/Cap Numbers</a></li>
             <li><a href="shirtnumbers.php">Shirt Numbers</a></li>
@@ -13,15 +14,11 @@
             <li><a href="halloffame.php">Hall of Fame</a></li>
             <li><a href="teamrules.php">Team Rules</a></li>
           </ul>
-          <?php if ($_SESSION["admin"] == 1) { ?>
-          <ul>
-            <li><a href="#">Admin Options:</a></li>
-            <li><a href="newsitems.php">Add/Edit News Items</a></li>
-            <li><a href="addmatches.php?seasonselect=2009/2010">Add Matches</a></li>
-            <li><a href="addplayers.php">Add Players</a></li>
-            <li><a href="grounds.php">Grounds</a></li>
-            <li><a href="teams.php">Teams</a></li>
-            <li><a href="jwxiplayerinfo.php">JWXI Player Info</a></li>
-          </ul>
-          <?php } ?>
+          <ul id="row2"></ul>
         </div>
+        
+        <script type="text/javascript">
+		function showRow2(selection) {
+			document.getElementById("row2").innerHTML = "<li><a href="WF-2012-SQUAD.php">2012 Squad</a></li>";
+		}
+        </script>
